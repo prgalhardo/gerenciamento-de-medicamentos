07f0a424fe873d4f6a3d6e54438774a499daebe3
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-03-01 20:37:05 -0300

    [reacoesAdversas] model - relacionamento tabelas e constructor

--- src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/model/ReacoesAdversas.java
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/model/ReacoesAdversas.java
@@ -1,9 +1,6 @@
 package com.priscila.memoryproject.gerenciamentomedicamento.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 /**
  * Entity: Reações Adversas.
@@ -15,4 +12,24 @@ public class ReacoesAdversas {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
   private String descricao;
+
+  @ManyToOne
+  @JoinColumn (name = "medicamento_id")
+  private Medicamentos medicamentos;
+
+  public ReacoesAdversas(String descricao) {
+    this.descricao = descricao;
+  }
+
+  /**
+   * Constructor.
+   */
+
+  public ReacoesAdversas(Integer id, String descricao, Medicamentos medicamentos) {
+    this.id = id;
+    this.descricao = descricao;
+    this.medicamentos = medicamentos;
+  }
+
+
 }
