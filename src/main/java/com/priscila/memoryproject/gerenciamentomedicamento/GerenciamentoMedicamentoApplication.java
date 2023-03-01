ec36a810197414f1f6a2fc00d3292a1c137f4193
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-02-28 22:43:34 -0300

    [ambiente] configuração do ambiente

---
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/GerenciamentoMedicamentoApplication.java
@@ -0,0 +1,16 @@
+package com.priscila.memoryproject.gerenciamentomedicamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe: Main.
+ */
+@SpringBootApplication
+public class GerenciamentoMedicamentoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GerenciamentoMedicamentoApplication.class, args);
+  }
+
+}
