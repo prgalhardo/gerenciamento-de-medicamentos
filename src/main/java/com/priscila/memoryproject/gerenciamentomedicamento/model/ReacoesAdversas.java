9121da90ae46586b8eb53741c1c5857a7870d20b
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-03-01 21:04:39 -0300

    [checkstyle] model - ReacoesAdversas

--- src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/model/ReacoesAdversas.java
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/model/ReacoesAdversas.java
@@ -1,6 +1,12 @@
 package com.priscila.memoryproject.gerenciamentomedicamento.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 /**
  * Entity: Reações Adversas.
