e1ba5917782e2e5af323c3c66848bb352a711ffd
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-03-01 21:16:51 -0300

    [medicamentos] model com atributos e relacionamento das tabelas

--- src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/model/Medicamentos.java
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/model/Medicamentos.java
@@ -1,18 +1,28 @@
 package com.priscila.memoryproject.gerenciamentomedicamento.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Entity: Medicamentos.
  */
 @Entity
+@Table(name = "medicamentos")
 public class Medicamentos {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
   private String numeroRegistroAnvisa;
+  private String nome;
+  private String dataValidade;
+  private String telefoneSac;
+  private String preco;
+  private String quantidadeComprimidos;
+  private String fabricante;
+
+  @OneToMany(mappedBy = "medicamentos")
+  private List<ReacoesAdversas> reacoesadversas = new ArrayList<>();
 
 }
