b3559a16c5a88bb47f436597337814ee217ba286
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-03-01 21:46:00 -0300

    [medicamentos] repository medicamentos

---
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/repository/MedicamentosRepository.java
@@ -0,0 +1,10 @@
+package com.priscila.memoryproject.gerenciamentomedicamento.repository;
+
+import com.priscila.memoryproject.gerenciamentomedicamento.model.Medicamentos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicamentosRepository extends JpaRepository<Medicamentos, Integer> {
+  boolean existsByNome(String nome);
+}
