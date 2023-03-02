4edc171f3d0b022bfa96a7f9799ae2f3e7e140e2
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-03-01 22:40:10 -0300

    [reacoesadversas] método deletar service

--- src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/service/ReacoesAdversasService.java
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/service/ReacoesAdversasService.java
@@ -1,8 +1,10 @@
 package com.priscila.memoryproject.gerenciamentomedicamento.service;
 
+import com.priscila.memoryproject.gerenciamentomedicamento.model.ReacoesAdversas;
 import com.priscila.memoryproject.gerenciamentomedicamento.repository.ReacoesAdversasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 /**
  * Classe: Reações Adversas.
@@ -11,4 +13,19 @@ import org.springframework.stereotype.Service;
 public class ReacoesAdversasService {
   @Autowired
   private ReacoesAdversasRepository reacoesAdversasRepository;
+
+  public List<ReacoesAdversas> findAll() {
+    return reacoesAdversasRepository.findAll();
+  }
+
+  /**
+   * Método: Deletar.
+   */
+  public void deletar(Integer id) {
+    if (reacoesAdversasRepository.existsById(id)) {
+      reacoesAdversasRepository.deleteById(id);
+    } else {
+      throw new ReacaoNaoEncontradaException("Reação adversa não existe");
+    }
+  }
 }
