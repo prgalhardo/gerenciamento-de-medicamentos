a8d086d31116116ada3b564ad97ac0cbb09de5e3
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-03-03 00:32:59 -0300

    [exception] medicamentos

---
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/exception/MedicamentoExistenteException.java
@@ -0,0 +1,5 @@
+package com.priscila.memoryproject.gerenciamentomedicamento.exception;
+
+public class MedicamentoExistenteException extends RuntimeException {
+  public MedicamentoExistenteException(String message) { super(message); }
+}
---
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/exception/MedicamentoNaoEncontradoException.java
@@ -0,0 +1,5 @@
+package com.priscila.memoryproject.gerenciamentomedicamento.exception;
+
+public class MedicamentoNaoEncontradoException extends RuntimeException {
+  public MedicamentoNaoEncontradoException(String message) { super(message); }
+}
