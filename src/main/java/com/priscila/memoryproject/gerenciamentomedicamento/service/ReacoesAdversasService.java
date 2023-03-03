1442ec314f1262b6438a97358218086dc44553ee
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-03-02 23:22:18 -0300

    [reaçoesadversas] service - método put

--- src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/service/ReacoesAdversasService.java
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/service/ReacoesAdversasService.java
@@ -1,45 +1,58 @@
 package com.priscila.memoryproject.gerenciamentomedicamento.service;
 
-import com.priscila.memoryproject.gerenciamentomedicamento.exception.ReacaoNaoEncontradaException;
+import com.priscila.memoryproject.gerenciamentomedicamento.exception.ReacaoExistenteException;
 import com.priscila.memoryproject.gerenciamentomedicamento.model.ReacoesAdversas;
 import com.priscila.memoryproject.gerenciamentomedicamento.repository.ReacoesAdversasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
+import java.util.Optional;
 
 /**
  * Classe: Reações Adversas.
  */
+
 @Service
 public class ReacoesAdversasService {
   @Autowired
   private ReacoesAdversasRepository reacoesAdversasRepository;
 
-/**
- * Método: Incluir
- */
+  /**
+   * Método: Incluir
+   */
 
-public ReacoesAdversas cadastrar(ReacoesAdversas reacoesAdversas) {
-  if (reacoesAdversasRepository.existsById(reacoesAdversas.getId())) {
-    throw new ReacaoExistenteException("Reação adversa existente!");
+  public ReacoesAdversas cadastrar(ReacoesAdversas reacoesAdversas) {
+    if (reacoesAdversasRepository.existsById(reacoesAdversas.getId())) {
+      throw new ReacaoExistenteException("Reação adversa existente!");
+    }
+    return reacoesAdversasRepository.save(reacoesAdversas);
   }
-  return reacoesAdversasRepository.save(reacoesAdversas);
-}
-
-//public Serie save(Serie serie) {
-//  if (serieRepository.existsByNome(serie.getNome())) {
-//    throw new SerieExistenteException("Série Existente");
-//  }
-//  return serieRepository.save(serie);
-//}
 
   /**
-   * Método: Deletar.
+   * Método: Atualizar
    */
-  public void deletar(Integer id) {
-    if (reacoesAdversasRepository.existsById(id)) {
-      reacoesAdversasRepository.deleteById(id);
-    } else {
-      throw new ReacaoNaoEncontradaException("Reação adversa não existe!");
+
+  public ReacoesAdversas atualizar(ReacoesAdversas reacoesAdversas, Integer id) {
+    Optional<ReacoesAdversas> reacoes = reacoesAdversasRepository.findById(id);
+    if (reacoes.isPresent()) {
+      ReacoesAdversas obj = reacoes.get();
+      if (reacoesAdversas.getDescricao() != null) {
+        obj.setDescricao(reacoesAdversas.getDescricao());
+      }
+      return reacoesAdversasRepository.save(reacoesAdversas);
     }
+    return null;
   }
+}
+
+//
+//  /**
+//   * Método: Deletar.
+//   */
+//  public void deletar(Integer id) {
+//    if (reacoesAdversasRepository.existsById(id)) {
+//      reacoesAdversasRepository.deleteById(id);
+//    } else {
+//      throw new ReacaoNaoEncontradaException("Reação adversa não existe!");
+//    }
+//  }
+//}
