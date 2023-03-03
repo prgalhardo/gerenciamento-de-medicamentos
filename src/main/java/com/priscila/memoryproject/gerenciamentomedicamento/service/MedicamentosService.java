849098f1f92f50c49b0a37e085d531d62f6678d0
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-03-03 00:16:35 -0300

    [medicamentos] service - método put

--- src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/service/MedicamentosService.java
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/service/MedicamentosService.java
@@ -1,10 +1,12 @@
 package com.priscila.memoryproject.gerenciamentomedicamento.service;
 
-import com.priscila.memoryproject.gerenciamentomedicamento.exception.ReacaoExistenteException;
 import com.priscila.memoryproject.gerenciamentomedicamento.model.Medicamentos;
+import com.priscila.memoryproject.gerenciamentomedicamento.model.ReacoesAdversas;
 import com.priscila.memoryproject.gerenciamentomedicamento.repository.MedicamentosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MedicamentosService {
@@ -19,5 +21,42 @@ public class MedicamentosService {
       throw new MedicamentoExistenteException("Medicamento existente!");
     }
     return medicamentosRepository.save(medicamentos);
+  }
+
+  /**
+   * Método: Atualizar.
+   */
+
+  public Medicamentos atualizar(Medicamentos medicamentos, Integer id) {
+    Optional<Medicamentos> medicamentosOptional = medicamentosRepository.findById(id);
+    if (medicamentosOptional.isPresent()) {
+      Medicamentos obj = medicamentosOptional.get();
+      if (medicamentos.getNumeroRegistroAnvisa() != null) {
+        obj.setNumeroRegistroAnvisa(medicamentos.getNumeroRegistroAnvisa());
+      }
+      if (medicamentos.getNome() != null) {
+        obj.setNome(medicamentos.getNome());
+      }
+      if (medicamentos.getDataValidade() != null) {
+        obj.setDataValidade(medicamentos.getDataValidade());
+      }
+      if (medicamentos.getTelefoneSac() != null) {
+        obj.setTelefoneSac(medicamentos.getTelefoneSac());
+      }
+      if (medicamentos.getTelefoneSac() != null) {
+        obj.setTelefoneSac(medicamentos.getTelefoneSac());
+      }
+      if (medicamentos.getPreco() != null) {
+        obj.setPreco(medicamentos.getPreco());
+      }
+      if (medicamentos.getQuantidadeComprimidos() != null) {
+        obj.setQuantidadeComprimidos(medicamentos.getQuantidadeComprimidos());
+      }
+      if (medicamentos.getFabricante() != null) {
+        obj.setFabricante(medicamentos.getFabricante());
+      }
+      return medicamentosRepository.save(medicamentos);
+    }
+    return null;
   }
 }
