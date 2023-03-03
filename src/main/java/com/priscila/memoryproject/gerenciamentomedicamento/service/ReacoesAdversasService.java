4f4cba2bfef8ff033c304954c24da56f7e0d9594
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-03-02 22:06:47 -0300

    [reacoesAdversas] service - método post

--- src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/service/ReacoesAdversasService.java
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/service/ReacoesAdversasService.java
@@ -1,5 +1,6 @@
 package com.priscila.memoryproject.gerenciamentomedicamento.service;
 
+import com.priscila.memoryproject.gerenciamentomedicamento.exception.ReacaoNaoEncontradaException;
 import com.priscila.memoryproject.gerenciamentomedicamento.model.ReacoesAdversas;
 import com.priscila.memoryproject.gerenciamentomedicamento.repository.ReacoesAdversasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,23 @@ public class ReacoesAdversasService {
   @Autowired
   private ReacoesAdversasRepository reacoesAdversasRepository;
 
-  public List<ReacoesAdversas> findAll() {
-    return reacoesAdversasRepository.findAll();
+/**
+ * Método: Incluir
+ */
+
+public ReacoesAdversas cadastrar(ReacoesAdversas reacoesAdversas) {
+  if (reacoesAdversasRepository.existsById(reacoesAdversas.getId())) {
+    throw new ReacaoExistenteException("Reação adversa existente!");
   }
+  return reacoesAdversasRepository.save(reacoesAdversas);
+}
+
+//public Serie save(Serie serie) {
+//  if (serieRepository.existsByNome(serie.getNome())) {
+//    throw new SerieExistenteException("Série Existente");
+//  }
+//  return serieRepository.save(serie);
+//}
 
   /**
    * Método: Deletar.
@@ -25,7 +40,6 @@ public class ReacoesAdversasService {
     if (reacoesAdversasRepository.existsById(id)) {
       reacoesAdversasRepository.deleteById(id);
     } else {
-      throw new ReacaoNaoEncontradaException("Reação adversa não existe");
+      throw new ReacaoNaoEncontradaException("Reação adversa não existe!");
     }
   }
-}
