6afe45f9e6e1334713c03f0a1589bd3f84077dec
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-03-06 03:33:30 -0300

    [teste] retorno da rota get reacoesAdversas com status ok

--- src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/exception/ReacaoNaoEncontradaException.java
+++ src/main/java/com/priscila/memoryproject/gerenciamentomedicamento/exception/ReacaoNaoEncontradaException.java
@@ -1,5 +1,8 @@
 package com.priscila.memoryproject.gerenciamentomedicamento.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Classe: Reação Não Encontrada Exception.
  */
--- src/test/java/com/priscila/memoryproject/gerenciamentomedicamento/GerenciamentoMedicamentoApplicationTests.java
+++ src/test/java/com/priscila/memoryproject/gerenciamentomedicamento/GerenciamentoMedicamentoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.priscila.memoryproject.gerenciamentomedicamento;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GerenciamentoMedicamentoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	/**
+	 * Teste: Rota GET Reações Adversas.
+	 */
+
+	@Test
+	void deveRetornarStatusOk() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/reacoesadversas"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
 }
